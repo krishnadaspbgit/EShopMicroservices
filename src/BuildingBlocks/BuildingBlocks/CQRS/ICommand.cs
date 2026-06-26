67489127718f10a546dc887fd437d661876e2cfe
DESKTOP-PVHHH33\Krishnadas <krishnadaspb88@gmail.com>
2026-06-26 22:46:55 +0530

    EShop-Project-26-June-2026

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand<out TResponse>:IRequest
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name,List<string> Category,string Description,string ImageFile,decimal Price);
+    public record CreateProductresult(Guid Id);
+    public class CreateProductHandler
+    {
+    }
+}
